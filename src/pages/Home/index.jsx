6124fa32dc0a8d18a55ac56a304459b2c12aaee9
payyer <quocanhle112@gmail.com>
2025-05-22 23:20:49 +0700

    feat/ app bar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,28 @@
+import { NavLink } from "react-router";
+import jmLogo from "../../assets/jm-logo.png";
+import SummerSale from "./partials/SummerSale";
 export default function Home() {
-  return <div>
-    <h1 className="text-3xl">Hello</h1>
-  </div>;
+  return (
+    <main>
+      <nav className="h-[94px] flex items-center  justify-center border-b-[1px] border-red-400">
+        <div className="h-[48px] max-w-[1180px] px-[20px] w-full flex items-center justify-between">
+          <NavLink to="/" end className="hover-effect">
+            <img
+              className="h-auto w-[102px]"
+              src={jmLogo}
+              height={48}
+              width={101}
+              alt="JM DRESS DESIGN"
+              loading="eager"
+              decoding="async"
+            />
+          </NavLink>
+
+          <div className="flex">
+            <SummerSale />
+          </div>
+        </div>
+      </nav>
+    </main>
+  );
 }
---
+++ src/pages/Home/partials/SummerSale.jsx
@@ -0,0 +1,27 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Link } from "react-router";
+
+export default function SummerSale() {
+  return (
+    <Link
+      to="/sale"
+      className="relative flex items-center gap-1 font-bold text-color-primary text-[14px] leading-[20px] group"
+    >
+      <span className="nav-text-hover">SUMMER SALE</span>
+      <MdKeyboardArrowDown />
+      <span className="absolute -top-[calc(100%+8px)] right-0 text-[10px] p-1 rounded-t-[4px] rounded-bl-[4px]  bg-red text-white leading-[14px]">
+        SALE
+      </span>
+      <ul className="absolute opacity-0 invisible top-[calc(100%+4px)] w-[150px] py-[10px] shadow-xs bg-white group-hover:opacity-100 group-hover:visible translate-y-[50%] group-hover:translate-y-[0%] transition-all duration-500 ease-in-out">
+        <Link to="/sale" className="summer-sale__li">
+          Đồng giá chỉ từ
+          <br /> 225k
+        </Link>
+        <Link to="/sale" className="summer-sale__li mt-2">
+          Siêu sale liền tay
+          <br /> 70%
+        </Link>
+      </ul>
+    </Link>
+  );
+}
